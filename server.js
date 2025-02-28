47aacbd8ccf2a5e226de288d57588fb649cdd636
JSPV220403 <162592820+JSPV220403@users.noreply.github.com>
2025-02-28 06:58:57 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ const server = createServer();
 const ioserver = new Server(server);
 
 
-const dbConfig = {
+const pool = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "Praveen@22",
-    database: "chat_db"
-};
+    database: "chat_db",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 
 async function initDB() {
-    const db = await mysql.createConnection(dbConfig);
+    const db = await pool.getConnection();
     await db.execute(`
         CREATE TABLE IF NOT EXISTS messages (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -25,34 +28,41 @@ async function initDB() {
         );
     `);
     console.log("Database initialized!");
-    await db.end();
+    db.release(); 
 }
 
-initDB(); 
+initDB();
+
 ioserver.on("connection", async (socket) => {
     console.log("A user connected!");
 
-    const db = await mysql.createConnection(dbConfig);
+    try {
+        const db = await pool.getConnection(); 
 
-    const [messages] = await db.query("SELECT sender, message FROM messages ORDER BY created_at ASC");
-    socket.emit("chat history", messages);
+        
+        const [messages] = await db.query("SELECT sender, message FROM messages ORDER BY created_at ASC");
+        socket.emit("chat history", messages);
 
-    socket.on("chat message", async (msg) => {
-        console.log(`Message: ${msg.text} from ${msg.sender}`);
+        socket.on("chat message", async (msg) => {
+            console.log(`Message: ${msg.text} from ${msg.sender}`);
 
-        const insertQuery = "INSERT INTO messages (sender, message) VALUES (?, ?)";
-        await db.execute(insertQuery, [msg.sender, msg.text]);
+            const insertQuery = "INSERT INTO messages (sender, message) VALUES (?, ?)";
+            await db.execute(insertQuery, [msg.sender, msg.text]);
 
-        ioserver.emit("chat message", msg);
-    });
+            socket.broadcast.emit("chat message", msg);
+        });
+
+        
+        db.release();
+
+    } catch (err) {
+        console.error("Database error:", err.message);
+    }
 
     socket.on("disconnect", () => {
         console.log("User disconnected");
     });
-
-    await db.end();
 });
-
 
 server.listen(3000, () => {
     console.log("Server started at port 3000");
